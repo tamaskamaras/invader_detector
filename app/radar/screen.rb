0c1b068fc77b06d26b999f7ba1cbc288d7d397f4
Tamás Kamarás <kamaras.tamas.viktor@gmail.com>
2022-04-02 13:26:43 +0200

    create Screen#to_frame

--- app/radar/screen.rb
+++ app/radar/screen.rb
@@ -2,14 +2,19 @@
 
 class Radar
   class Screen < Radar
+    def each_frame(frame_width, frame_height)
+      body.each_with_index do |row, row_index|
+        row.each_index do |pixel_index|
+          yield(to_frame(row_index, pixel_index, frame_width, frame_height))
+        end
+      end
+    end
 
     private
 
-    def each_frame(frame_width, frame_height)
-      body.each_index do |row_index|
-        row.each_index do |pixel_index|
-          yeild()
-        end
+    def to_frame(row_index, pixel_index, frame_width, frame_height)
+      body[row_index...(row_index + frame_height)].map do |row|
+        row[pixel_index...(pixel_index + frame_width)]
       end
     end
   end
