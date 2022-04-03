c709afb89dc07346471c6d23cefefee0c9536fc6
Tamás Kamarás <kamaras.tamas.viktor@gmail.com>
2022-04-03 20:13:23 +0200

    introduce shared_examples

--- spec/app/monitor_spec.rb
+++ spec/app/monitor_spec.rb
@@ -2,27 +2,34 @@
 
 require 'spec_helper'
 
+RSpec.shared_examples 'a radar scanner' do
+  it 'returns expected invader data', :aggregate_failures do
+    expect(monitor.detect(invader1)).to eq(expected_invader1_data)
+    expect(monitor.detect(invader2)).to eq(expected_invader2_data)
+    expect(monitor.detect([invader1, invader2])).to eq(expected_invader1_data + expected_invader2_data)
+  end
+end
+
 RSpec.describe Monitor do
   let(:invader1) { Radar::Invader.new('spec/fixtures/radar/invaders/invader_1') }
   let(:invader2) { Radar::Invader.new('spec/fixtures/radar/invaders/invader_2') }
+  let(:expected_invader1_data) { [] }
+  let(:expected_invader2_data) { [] }
+  let(:tolerance) { 0 }
 
   subject(:monitor) do
-    described_class.new(screen)
+    described_class.new(screen, tolerance: tolerance)
   end
 
   context 'when screen is empty' do
     let(:screen) { Radar::Screen.new('spec/fixtures/radar/screens/empty') }
 
-    it 'returns an empty Array', :aggregate_failures do
-      expect(monitor.detect(invader1)).to eq([])
-      expect(monitor.detect(invader2)).to eq([])
-      expect(monitor.detect([invader1, invader2])).to eq([])
-    end
+    it_behaves_like 'a radar scanner'
   end
 
   context 'when screen clearly displays 1 invader' do
     let(:screen) { Radar::Screen.new('spec/fixtures/radar/screens/with_invader_1') }
-    let(:invader1_data) do
+    let(:expected_invader1_data) do
       [
         {
           size: invader1.size,
@@ -31,16 +38,12 @@ RSpec.describe Monitor do
       ]
     end
 
-    it 'returns the size and location of invader1', :aggregate_failures do
-      expect(monitor.detect(invader1)).to match_array(invader1_data)
-      expect(monitor.detect(invader2)).to eq([])
-      expect(monitor.detect([invader1, invader2])).to match_array(invader1_data)
-    end
+    it_behaves_like 'a radar scanner'
   end
 
   context 'when screen clearly displays 4 invaders with_multiple_invaders' do
     let(:screen) { Radar::Screen.new('spec/fixtures/radar/screens/with_multiple_invaders') }
-    let(:invader1_data) do
+    let(:expected_invader1_data) do
       [
         {
           size: invader1.size,
@@ -52,7 +55,7 @@ RSpec.describe Monitor do
         }
       ]
     end
-    let(:invader2_data) do
+    let(:expected_invader2_data) do
       [
         {
           size: invader2.size,
@@ -65,26 +68,19 @@ RSpec.describe Monitor do
       ]
     end
 
-    it 'returns the size and location of the invaders', :aggregate_failures do
-      expect(monitor.detect(invader1)).to match_array(invader1_data)
-      expect(monitor.detect(invader2)).to match_array(invader2_data)
-      expect(monitor.detect([invader1, invader2])).to match_array(invader1_data + invader2_data)
-    end
+    it_behaves_like 'a radar scanner'
   end
 
   context 'when screen displays invaders with noise' do
     let(:screen) { Radar::Screen.new('spec/fixtures/radar/screens/with_noise_and_invaders') }
 
     context 'when tolerance is 0' do
-      it 'returns the size and location of the invaders', :aggregate_failures do
-        expect(monitor.detect(invader1)).to eq([])
-        expect(monitor.detect(invader2)).to eq([])
-        expect(monitor.detect([invader1, invader2])).to eq([])
-      end
+      it_behaves_like 'a radar scanner'
     end
 
     context 'when tolerance is 10' do
-      let(:invader2_data) do
+      let(:tolerance) { 10 }
+      let(:expected_invader2_data) do
         [
           {
             size: invader2.size,
@@ -93,19 +89,12 @@ RSpec.describe Monitor do
         ]
       end
 
-      subject(:monitor) do
-        described_class.new(screen, tolerance: 10)
-      end
-
-      it 'returns the size and location of the invaders', :aggregate_failures do
-        expect(monitor.detect(invader1)).to eq([])
-        expect(monitor.detect(invader2)).to eq(invader2_data)
-        expect(monitor.detect([invader1, invader2])).to eq(invader2_data)
-      end
+      it_behaves_like 'a radar scanner'
     end
 
     context 'when tolerance is 20' do
-      let(:invader1_data) do
+      let(:tolerance) { 20 }
+      let(:expected_invader1_data) do
         [
           {
             size: invader1.size,
@@ -121,7 +110,7 @@ RSpec.describe Monitor do
           }
         ]
       end
-      let(:invader2_data) do
+      let(:expected_invader2_data) do
         [
           {
             size: invader2.size,
@@ -142,15 +131,7 @@ RSpec.describe Monitor do
         ]
       end
 
-      subject(:monitor) do
-        described_class.new(screen, tolerance: 20)
-      end
-
-      it 'returns the size and location of the invaders', :aggregate_failures do
-        expect(monitor.detect(invader1)).to eq(invader1_data)
-        expect(monitor.detect(invader2)).to eq(invader2_data)
-        expect(monitor.detect([invader1, invader2])).to eq(invader1_data + invader2_data)
-      end
+      it_behaves_like 'a radar scanner'
     end
   end
 end
